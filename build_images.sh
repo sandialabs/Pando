347087bbbb68f6645ac66d3ea61e6fb47458606e
Peter Clark <pgclark@iastate.edu>
2026-05-07 23:31:12 -0500

    Configuration for ISU Nova HPC

--- build_images.sh
+++ build_images.sh
@@ -1,33 +1,20 @@
 #!/bin/bash -ex
 
-P="${P:-podman}"        # Run with P=docker ./build_images.sh to change
+SCRATCH_DIR="${SCRATCH_DIR:-/scratch}"
+if [ ! -d "$SCRATCH_DIR" ] || [ ! -w "$SCRATCH_DIR" ]; then
+    echo "ERROR: SCRATCH_DIR='$SCRATCH_DIR' is not a writable directory" >&2
+    exit 1
+fi
 
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 pushd "${SOURCE_DIR}"
 
-function build_sif() {
-    build_im "$@"
-    mkdir -p sifs
-    rm -vf sifs/$1.sif
-    "$P" image save -o $1.tar $1
-    singularity build sifs/$1.sif "docker-archive://$PWD/$1.tar"
-    rm -v $1.tar
-}
+mkdir -p sifs
 
-function build_im() {
-    "$P" image build -t $1 -f env/$1 $2
-}
-
-build_im pando_base_os env
-build_sif pando_dev env
-build_im pando_build .
-build_im pando_run env
-build_sif pando_proxy env
-build_sif pando_pardb env
-build_sif pando_client env
-build_sif pando_mon env
-build_sif pando_top env
-build_sif pando_python env
-build_sif pando_jupyter env
+# Empty APPTAINER_BINDPATH during build to avoid host module-injected binds
+# interfering with %post; --fakeroot needed because pgclark is not in /etc/subuid
+APPTAINER_BINDPATH="" apptainer build --fakeroot \
+    --bind "$SCRATCH_DIR:/scratch" \
+    sifs/pando_dev.sif env/pando_dev.def
 
 popd
